b800c58429261a9514b299ace2561c7644399ac2
shailendra153 <kushwahshailendra12345@gmail.com>
2022-04-20 14:03:42 +0530

    's'

--- src/app/view-product/view-product.component.ts
+++ src/app/view-product/view-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../product.service';
+import{UpdateDataService} from '../update-data.service'
 
 @Component({
   selector: 'app-view-product',
@@ -10,11 +11,26 @@ import { ProductService } from '../product.service';
 export class ViewProductComponent implements OnInit {
   productList:any;
 
-  constructor(private service:ProductService,private router:Router) {
+  constructor(private service:ProductService,private route:Router,private update:UpdateDataService) {
     this.service.viewProduct().subscribe(data=>{
       this.productList=data;
     })
    }
+   deleteProduct(productId:String,index:Number){
+    if(confirm("Are you sure")){
+     this.service.deleteProduct(productId).subscribe(data=>{
+       if(data){
+         console.log(data);
+         this.productList.splice(index,1);
+       }
+     })
+    }
+    
+}
+updateCategory(data:any){
+   this.update.setData(data);
+     this.route.navigate(["update-product"]);
+    }
 
   ngOnInit(): void {
   }
